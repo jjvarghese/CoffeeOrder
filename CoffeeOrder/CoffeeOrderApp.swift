efa8b98d21aa24ef92062a10d2a92b1cd5d696d4
Joshua J Varghese <joshua.varghese@curalie.com>
2023-03-22 10:54:52 +0100

    Initial Commit

---
+++ CoffeeOrder/CoffeeOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeOrderApp.swift
+//  CoffeeOrder
+//
+//  Created by Joshua Varghese on 22.03.23.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
